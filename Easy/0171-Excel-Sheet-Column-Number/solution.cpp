b269e330c5f5dded4ad7c70a3a83db498f30933e
Oblutack <kamenjas.evvel@gmail.com>
2025-12-07 14:20:27 +0100

    Give the column title in the excel sheet, I return the corresponding column number.

---
+++ Easy/0171-Excel-Sheet-Column-Number/solution.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+class Solution {
+public:
+    int titleToNumber(std::string columnTitle) {
+
+        long long result = 0;
+
+        for (char c : columnTitle) {
+
+
+            int value = c - 'A' + 1;
+
+
+            result = result * 26 + value;
+        }
+
+        return (int)result;
+    }
+};
